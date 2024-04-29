31430916f1177a0e8a57d85c5d974476eea6e3a6
webHikari <q5503212@gmail.com>
2024-04-29 23:32:28 +0300

    +RegisterForm

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -3,13 +3,13 @@ import WidgetContainer from "@shared/WidgetContainer/WidgetContainer";
 import Input from "@shared/Input/Input";
 
 export default function Dashboard() {
-    return (
-        <>
-            <Input value={"Insert some text"} styleType={"Input1"} />
-            <Header />
-            <WidgetContainer>
-                <p>123</p>
-            </WidgetContainer>
-        </>
-    );
+  return (
+    <>
+      <Input placeholderValue={"Insert some text"} styleType={"Input1"} />
+      <Header />
+      <WidgetContainer>
+        <p>123</p>
+      </WidgetContainer>
+    </>
+  );
 }
